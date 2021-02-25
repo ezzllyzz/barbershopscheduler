1979ad60505ffd25e702eaf59778078d47c240ef
Ziqing Yuan <78901885+ziqingyuan@users.noreply.github.com>
2021-02-25 04:51:39 -0800

    Add feature for date,  Attempt to solve problem1

--- main.hs
+++ main.hs
@@ -4,67 +4,142 @@ import Data.Ord (comparing)
 import Data.Time
 import Data.Typeable
 import System.IO
+import System.Directory
 
 
 test = [FreeSlot {time = Ten},TakenSlot {time = Twelve, name = "Amy"}]
 
+-- Start function
+run :: IO ()
 run = 
     do
-        
         hSetBuffering stdin LineBuffering
-        openingTerminalText
+        putStrLn("")
+        putStrLn("************************\n")
+        putStrLn("Hello! Welcome to Barber Scheduler.")
+        chooseDate
+
+
+-- choose the booking date
+chooseDate :: IO ()
+chooseDate = 
+    do
+        putStrLn("What date do you plan to come to our shop? In form of YYYY-MM-DD")
+        putStrLn("~~~NOTE that we are closed every tuesday~~~")
+        datePreferedStr <- getLine 
+        if (isYearFormat datePreferedStr) 
+            then do
+                c <- getCurrentTime 
+                let (ty,tm,td) = toGregorian $ utctDay c
+                let today = (fromGregorian ty tm td)
+                let datePrefered = convertToDate datePreferedStr
+                let dayoFWeek = dayOfWeek datePrefered
+                
+                if datePrefered < today
+                    then do
+                            putStrLn("The date you enter is invalid, please enter from today after") 
+                            chooseDate
+                else if (dayoFWeek == Tuesday)
+                    then do
+                            putStrLn("We are not working this day, please enter another valid date") 
+                            chooseDate      
+                else 
+                    do
+                        chooseBaber datePreferedStr
+        else if (elem datePreferedStr ["quit", "QUIT", "Q"])
+            then do 
+                putStrLn("You are leaving the booking system....")
+        else 
+            do
+                putStrLn("The date you enter is invalid, please enter another day") 
+                chooseDate
+
+
+-- take the date and check for barber
+chooseBaber :: String -> IO ()
+chooseBaber date = 
+    do
         putStrLn("There are two barbers in our barber shop: Tony and Tom")
-        putStrLn("Enter the name of your perferred barber:")
+        putStrLn("Please enter the name of your perferred barber to see his/her schedule:")
         ans <- getLine
         if (elem ans ["Tony", "tony", "TONY"])
-            then do
-                printSchedule "tony"
+            then do 
+                printSchedule "tony" date
         else if (elem ans ["Tom", "tom", "TOM"])
             then do
-                printSchedule "tom"
+                printSchedule "tom" date
+        else if (elem ans ["quit", "QUIT", "Q"])
+            then do 
+                putStrLn("You are leaving the booking system....")
         else 
-            putStrLn("banana") 
+            do
+                putStrLn("There are no such barber in our shop, please rechoose your baber") 
+                chooseBaber date
 
 
-
--- take the csv file by barber's name then process new schedule booking
-printSchedule :: String -> IO ()
-printSchedule name =
+-- take the csv file at preferred date then show the slotlist as table
+printSchedule :: [Char] -> [Char] -> IO ()
+printSchedule name date =
     do
-        file <- readCsv (name ++ ".csv")
-        let slotList = readCsvToSlot file
-        -- note slotList = [FreeSlot {time = Ten},TakenSlot {time = Twelve, name = "Amy"}, ...]
-        let printableSlot = toPrintableString slotList
-        putStrLn(name ++ " has the following schedule: ")
-        putStrLn("---------------------------------------------")
-        putStrLn(printableSlot)
+        let fileName = name ++ date ++ ".csv"
+        doExist <- (doesFileExist fileName)
+        if doExist
+            then do
+                file <- readCsv (fileName)
+                let slotList = readCsvToSlot file
+                -- note slotList = [FreeSlot {time = Ten},TakenSlot {time = Twelve, name = "Amy"}, ...]
+                let printableSlot = toPrintableString slotList
+                putStrLn(name ++ " has the following schedule: ")
+                putStrLn("---------------------------------------------")
+                putStrLn(printableSlot)
+                checkSchedule name date slotList 
+                
+        else 
+            do
+                file <- readCsv (name ++ ".csv")
+                let slotList = readCsvToSlot file
+                -- note slotList = [FreeSlot {time = Ten},TakenSlot {time = Twelve, name = "Amy"}, ...]
+                let printableSlot = toPrintableString slotList
+                putStrLn(name ++ " has the following schedule: ")
+                putStrLn("---------------------------------------------")
+                putStrLn(printableSlot)
+                checkSchedule name date slotList
+                
+
+
+
+-- take the slotlist of the asked day by barber's name then process new schedule booking for time
+checkSchedule :: [Char] -> [Char] -> [TimeSlot]  -> IO ()
+checkSchedule name date slotList= 
+    do 
         putStrLn("When whould you like to design your hair (please enter in form of xx:00)")
         inputTimeAsString <- getLine 
         let timeWanted = convertStringtoTime inputTimeAsString
         if checkAva slotList timeWanted
             then do 
-                processBooking name timeWanted slotList
+                    processBooking name timeWanted slotList date
         else do 
-            -- error "the time you pick is not avaliable, please choose another time"
-            putStrLn("The time you pick is unavailable today.")
-            putStrLn("...")
-            let timePick = take 2 inputTimeAsString -- "12"
-            let timePicked = read timePick::Integer
-
-            if (timePicked > 10 || timePicked < 17)
-                then do
-                    putStrLn("Trying to find the nearest available time today for " ++ name ++ " ...")
-                    putStrLn("...")
-                    checkNearest name slotList timePicked timePicked 
-            else do
-                putStrLn("No time is available today for " ++ name ++ ", please check other barbers.")
+                -- error "the time you pick is not avaliable, please choose another time"
+                putStrLn("The time you pick is unavailable today.")
                 putStrLn("...")
+                let timePick = take 2 inputTimeAsString -- "12"
+                let timePicked = read timePick::Integer
+
+                if (timePicked > 10 || timePicked < 17)
+                    then do
+                        putStrLn("Trying to find the nearest available time today for " ++ name ++ " ...")
+                        putStrLn("...")
+                        checkNearest name slotList timePicked timePicked date
+                else do
+                        putStrLn("This is not the working time for " ++ name ++ ", please check other avaliable time.")
+                        putStrLn("...")
+
 
         
 -- find the nearest availble time to schedule, process booking schedule
 -- when entered time is not availble for the selected barber
-checkNearest :: String -> [TimeSlot] -> Integer -> Integer -> IO ()
-checkNearest name slotList earlier later = 
+checkNearest :: String -> [TimeSlot] -> Integer -> Integer -> String -> IO ()
+checkNearest name slotList earlier later date = 
     if (earlier > 10 || later < 17)
         then do
             let sete = earlier-1
@@ -90,13 +165,15 @@ checkNearest name slotList earlier later =
                     let timeWanted = convertStringtoTime inputTimeAsString
                     if checkAva slotList timeWanted
                         then do 
-                            processBooking name timeWanted slotList
+                            processBooking name timeWanted slotList date
                     else do 
                         putStrLn("Thank you for using barber shop scheduler. ")
             else do
-                checkNearest name slotList sete setl
+                putStrLn("No time is available today for " ++ name ++ ", please check other date or baber.")
+                
     else do
-            putStrLn("No time is available today for " ++ name ++ ", please check other barbers.")
+            putStrLn("No time is available today for " ++ name ++ ", please check other date or barbers.")
+            
 
 
 -- takes a list of time and a schedule csv of a barber then produce a list of time which are available in the csv file
@@ -105,8 +182,8 @@ existFreeSlot [] slotList = []
 existFreeSlot lst slotList = filter (checkAva slotList) lst
 
 -- takes barber's name, time needed to be scheduled and list of schedules in csv file then save the new schedule to the csv file
-processBooking :: String -> Time -> [TimeSlot] -> IO ()
-processBooking name timeWanted slotList = 
+
+processBooking name timeWanted slotList date = 
     do
         putStrLn("What's your prefered name(Do not leave it empty)")
         preferredName <- getLine
@@ -115,12 +192,14 @@ processBooking name timeWanted slotList =
         let newSchedule = addNewBooking slotList newSlot
         let newStringSchedule = toCsv newSchedule
         when (length newStringSchedule > 0) $
-            writeFile (name ++ ".csv") newStringSchedule
-        putStrLn ("reschedule complete, here's the new schedule for " ++ name)
-        newfile <- readCsv (name ++ ".csv")
+            writeFile (name ++ date ++ ".csv") newStringSchedule
+        putStrLn ("reschedule complete, here's the new schedule for " ++ name ++ " on " ++ date)
+        newfile <- readCsv (name ++ date ++ ".csv")
         let newSlotList = readCsvToSlot newfile
         let newPrintableSlot = toPrintableString newSlotList
         putStrLn(newPrintableSlot)
+
+
 
 -- convert Time to String which is easier to read
 convertTimetoString :: Time -> String
@@ -168,7 +247,7 @@ data Time = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine 
            | Seventeen | Eighteen | Nineteen | Twenty 
             deriving (Ord, Eq, Show, Read, Typeable)
 
-data TimeSlot = TakenSlot { time :: Time, name :: String}   
+data TimeSlot = TakenSlot { time :: Time, name :: [Char]}   
                 | FreeSlot { time :: Time }
                 deriving (Ord, Eq, Show)
 
@@ -224,7 +303,7 @@ toString :: [TimeSlot] -> [String]
 toString [] = [""]
 toString lst = map printSlot lst 
     where 
-        printSlot (TakenSlot time name) = (show time) ++ "," ++ (show name)
+        printSlot (TakenSlot time name) = (show time) ++ "," ++  name
         printSlot (FreeSlot time) = (show time) 
 
 --take a list of timeslot and merge tham to a single string for csv file
@@ -238,13 +317,36 @@ mergeWith str [a] = a
 mergeWith str (h:t) =
     h ++ str ++ (mergeWith str t)
 
--- produce opening welcome text
-openingTerminalText :: IO ()
-openingTerminalText = 
-    do
-        putStrLn("")
-        putStrLn("************************\n")
-        putStrLn("Hello! Welcome to Barber Scheduler.")
+
+-- convert string inform of "YYYY-MM-DD" to date type (fromGregorian YYYY MM DD)
+convertToDate :: String -> Day
+convertToDate str = 
+    let
+        dateList = splitSep (== '-') str
+        yearNum = read (dateList!!0) :: Integer 
+        monthNum = read (dateList!!1) :: Int
+        dayNum = read (dateList!!2) :: Int  
+    in (fromGregorian yearNum monthNum dayNum)
+    
+
+-- check if the string  is in format of "YYYY-MM-DD" and is a valid date
+isYearFormat :: [Char] -> Bool
+isYearFormat [] = False 
+isYearFormat str 
+    | length lst == 3 = validComb year month date 
+    | otherwise = False 
+    where 
+        lst = splitSep (== '-') str
+        year = read (lst!!0) :: Integer 
+        month = read (lst!!1) :: Int
+        date = read (lst!!2) :: Int
+        validComb y m d 
+            | (elem m [1, 3, 5, 7, 8, 10, 12]) = d >= 1 && d <= 31
+            | (elem m [4, 6, 9, 11]) = d >= 1 && d <= 30
+            | m == 2 = d >= 1 && d <= 29
+            | otherwise = False 
+
+
 
 -- credit to David Poole, Homework 3 Question 3
 splitSep :: (a -> Bool) -> [a] -> [[a]]
@@ -259,3 +361,4 @@ readCsv fileName =
     do
         file <- readFile fileName
         return [splitSep (== ',') line | line <- splitSep (== '\n') file]
+
